1046a1c56195635bf15f22ae393220efcc67e3dc
Micah <stalberm@oregonstate.edu>
2023-12-06 18:08:55 -0800

    Update home text

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,45 +3,33 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="m-4 flex-fill d-flex flex-column">
-      {/* Single row with three cards */}
       <div className="row mx-0 flex-grow-1">
-        {/* Card 1: Rehabilitation Programs */}
         <div className="col-md-4">
           <div className="card h-100">
             <div className="card-body">
               <h2 className="card-title text-center">Recent Programs</h2>
               <p className="card-text">
-                Explore and create customized rehabilitation programs for your
-                patients. Include exercises, set goals, and track progress to
-                optimize their recovery.
+                Maybe list of cards of fake programs
               </p>
             </div>
           </div>
         </div>
-
-        {/* Card 2: Client Management */}
         <div className="col-md-4">
           <div className="card h-100">
             <div className="card-body">
               <h2 className="card-title text-center">Recent Clients</h2>
               <p className="card-text">
-                Efficiently manage your clients' information. Keep track of
-                personal details, medical history, and assessment results to
-                provide personalized and effective care.
+                Maybe list of cards of fake clients
               </p>
             </div>
           </div>
         </div>
-
-        {/* Card 3: Schedule */}
         <div className="col-md-4">
           <div className="card h-100">
             <div className="card-body">
               <h2 className="card-title text-center">Schedule</h2>
               <p className="card-text">
-                Efficiently manage your clients' information. Keep track of
-                personal details, medical history, and assessment results to
-                provide personalized and effective care.
+                Put some stuff here about upcoming patients or something
               </p>
             </div>
           </div>
