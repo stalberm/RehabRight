10f13739c22091b9394558168c553e422be67723
Baleria Reyes <breyes815@gmail.com>
2023-12-10 21:23:35 -0800

    Add set and rep state increment and decrement functionality

--- src/routes/CreateProgram.jsx
+++ src/routes/CreateProgram.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 
 export default function CreateProgram({ selectedExercises }) {
   return (
@@ -24,6 +25,15 @@ export default function CreateProgram({ selectedExercises }) {
 }
 
 function ExerciseEntry({ exercise }) {
+  const [reps, setReps] = useState(0);
+  const [sets, setSets] = useState(0);
+
+  const incrementReps = () => setReps(reps + 1);
+  const decrementReps = () => setReps(reps > 0 ? reps -1 : 0);
+
+  const incrememntSets = () => setSets(sets + 1);
+  const decrementSets = () => setSets(sets > 0 ? sets - 1 : 0);
+
   return (
     <div className="card mb-4" style={{ height: '25vh' }}>
       <div className="row g-0 align-items-center h-100">
@@ -35,12 +45,14 @@ function ExerciseEntry({ exercise }) {
           />
         </div>
         <div className="col-md-4 d-flex flex-column align-items-center">
-          <button className="btn btn-primary mb-2" style={{ width: '50px', height: '50px', flex: '1' }}>+</button>
-          <button className="btn btn-primary" style={{ width: '50px', height: '50px', flex: '1' }}>-</button>
+          <button onClick={incrementReps} className="btn btn-primary mb-2" style={{ width: '50px', height: '50px', flex: '1' }}>+</button>
+          <div>Reps: {reps}</div>
+          <button onClick={decrementReps} className="btn btn-primary" style={{ width: '50px', height: '50px', flex: '1' }}>-</button>
         </div>
         <div className="col-md-4 d-flex flex-column align-items-center">
-          <button className="btn btn-primary mb-2" style={{ width: '50px', height: '50px', flex: '1' }}>+</button>
-          <button className="btn btn-primary mb-2" style={{ width: '50px', height: '50px', flex: '1' }}>-</button>
+          <button onClick={incrememntSets} className="btn btn-primary mb-2" style={{ width: '50px', height: '50px', flex: '1' }}>+</button>
+          <div>Sets: {sets}</div>
+          <button onClick={decrementSets} className="btn btn-primary mb-2" style={{ width: '50px', height: '50px', flex: '1' }}>-</button>
           <button className="btn btn-danger" style={{ width: '100px', height: '50px', flex: '1' }}>Delete</button>
         </div>
       </div>
